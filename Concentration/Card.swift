c4d1c74f08b65f62a20df0726a2e358c41dace47
Gina Sprint <gsprint@ginas-macbook-pro.local>
2018-06-21 21:18:32 -0700

    Add MVC architecture and initial game play logic

---
+++ Concentration/Card.swift
@@ -0,0 +1,47 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Gina Sprint on 6/21/18.
+//  Copyright © 2018 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+// structs and classes are mostly the same
+
+// difference between struct and class
+// 2 major
+// 1. structs: no inheritance
+// 2. structs: value types, classes: reference types
+// value put it in array, assign to another variable, pass into function -> COPIED!
+// Array, String, Int, Dictionary are structs: COPIED
+// copy on right semantics: only copies values when value is going to change
+// references live in the heap, not passing around the objecct -> passing around pointers
+
+
+struct Card {
+    var isFaceUp = false
+    var matched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    // static method belongs to type, not to instance
+    // utility function tied to the type
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        // don't need Card. because we are in static method
+        // accessing static property
+        return Card.identifierFactory
+    }
+    
+    // struct gets free init
+    // which initializes all properties
+    
+    // inits are the one method that usually
+    // has the same internal and external parameter names
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -18,7 +18,10 @@ class ConcentrationViewController: UIViewController {
     // array of UIButtons
     @IBOutlet var cardButtons: [UIButton]!
     
-    let emojis: Array<String> = ["🍉", "🥝", "🍉", "🥝"]
+    // lazy doesnt actually initialize until some one tries to use game
+    // lazy cannot have property observer (the didSet)
+    lazy var game = ConcentrationModel(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+
     
     var flipCount: Int = 0 {
         didSet {
@@ -32,24 +35,54 @@ class ConcentrationViewController: UIViewController {
         print("touched!")
         if let cardNumber = cardButtons.index(of: sender) {
             print(cardNumber)
-            flipCard(withEmoji: emojis[cardNumber] , on: sender)
+            game.chooseCard(at: cardNumber)
+            flipCount += 1
+            updateViewFromModel()
         }
         else {
             print("chosen card was not in cardButtons")
         }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        flipCount += 1
-        
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControlState.normal)
-            button.backgroundColor = UIColor.orange
-        }
-        else {
-            button.setTitle(emoji, for: UIControlState.normal)
-            button.backgroundColor = UIColor.lightGray
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = UIColor.white
+            }
+            else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.matched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
         }
     }
+    
+    var emoji = Dictionary<Int, String>()
+    var emojiChoices = ["🍉", "🥝", "🍇", "🍋", "🍓", "🥥", "🍊", "🍏"]
+    
+    func emoji(for card: Card) -> String {
+        
+        // swift never does any automatic type conversion
+        // use type initializers
+        if emoji[card.identifier] == nil {
+            if emojiChoices.count > 0 {
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                // remove(at:) returns the thing it removes
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            }
+            
+        }
+//        if emoji[card.identifier] != nil {
+//            return emoji[card.identifier]!
+//        }
+//
+//        return "?"
+        
+        // return emoji[card.identifier] if not nil, otherwise return "?"
+        return emoji[card.identifier] ?? "?"
+    }
+    
 }
 
